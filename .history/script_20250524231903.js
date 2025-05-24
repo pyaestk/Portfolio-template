29d2f65ae78fecdd4fbd09cad8a825efed370053
pyaesonethantkyaw206@gmail.com <pyaesonethantkyaw206@gmail.com>
2025-05-25 00:27:24 +0700

    :update android logo mascot

---
+++ .history/script_20250524231903.js
@@ -0,0 +1,60 @@
+/* on scroll animation */
+function reveal() {
+  var reveals3 = document.querySelectorAll(".reveal3");
+  for (var i = 0; i < reveals3.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals3[i].getBoundingClientRect().top;
+    var elementVisible = 50;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals3[i].classList.add("active3");
+    } else {
+      reveals3[i].classList.remove("active3");
+    }
+  }
+}
+// Also run reveal() on scroll
+window.addEventListener("scroll", reveal);
+
+//download CV
+document.getElementById("downloadBtn").addEventListener("click", function () {
+  const fileUrl = "my cv.pdf"; // PDF file in the same folder
+
+  const a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = "CV.pdf"; // The filename when downloaded
+
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+});
+
+
+var lang = {
+  "html": "100%",
+  "css": "90%",
+  "javascript": "70%",
+  "php": "55%",
+  "angular": "65%"
+};
+
+var multiply = 4;
+
+$.each( lang, function( language, pourcent) {
+
+  var delay = 700;
+  
+  setTimeout(function() {
+    $('#'+language+'-pourcent').html(pourcent);
+  },delay*multiply);
+  
+  multiply++;
+
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const logo = document.getElementById("android-logo");
+    setTimeout(() => {
+        logo.style.display = 'none';
+    }, 4000); // after 4 seconds (fade in + wave + fade out)
+});
---
+++ .history/script_20250524232031.js
@@ -0,0 +1,41 @@
+/* on scroll animation */
+function reveal() {
+  var reveals3 = document.querySelectorAll(".reveal3");
+  for (var i = 0; i < reveals3.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals3[i].getBoundingClientRect().top;
+    var elementVisible = 50;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals3[i].classList.add("active3");
+    } else {
+      reveals3[i].classList.remove("active3");
+    }
+  }
+}
+// Also run reveal() on scroll
+window.addEventListener("scroll", reveal);
+
+//download CV
+document.getElementById("downloadBtn").addEventListener("click", function () {
+  const fileUrl = "my cv.pdf"; // PDF file in the same folder
+
+  const a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = "CV.pdf"; // The filename when downloaded
+
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const logo = document.getElementById("android-logo");
+    logo.classList.add("animate-on-load");
+
+    setTimeout(() => {
+        logo.style.display = 'none';
+    }, 4000); // after slide in (0.8s) + wave (2s) + slide out (0.8s)
+});
